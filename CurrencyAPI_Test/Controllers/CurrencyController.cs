fc7f708623b9097c5466b447b0ebab9e53229525
Evleksey <alzer35@gmail.com>
2022-02-10 01:13:06 +0300

    Add project files.

---
+++ CurrencyAPI_Test/Controllers/CurrencyController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json.Nodes;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CurrencyAPI_Test.Controllers
+{
+    
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+
+        private readonly ILogger<CurrencyController> _logger;
+
+        public CurrencyController(ILogger<CurrencyController> logger)
+        {
+            _logger = logger;
+        }
+
+        protected JsonNode? Update()
+        {
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                client.BaseAddress = new Uri("https://www.cbr-xml-daily.ru/");
+                HttpResponseMessage response = client.GetAsync("daily_json.js").Result;
+                //response.EnsureSuccessStatusCode();
+                var res = response.Content.ReadAsStringAsync().Result;
+                return JsonNode.Parse(res);
+            }
+        }
+
+        [Route("/currencies/{take}/{skip}")]
+        [HttpGet]
+        public JsonObject[] Get(int take = 10, int skip = 0)
+        {            
+            var x = Update()?["Valute"].AsObject().ToList().Skip(skip).Take(take);
+            var res = x.Select(x => new JsonObject { [x.Key] = JsonNode.Parse(x.Value.AsObject().ToJsonString())}).ToArray();
+
+            return res;
+        }
+
+        [Route("/currencie/{id}")]
+        [HttpGet]
+        public JsonNode? Get(string id)
+        {
+           
+
+            var x = Update()?["Valute"].AsObject().ToList();
+            return  x.Where(n => n.Key == id).FirstOrDefault().Value;
+        }
+
+       
+    }
+}
---
+++ CurrencyAPI_Test/Program.cs
@@ -0,0 +1,28 @@
+using System.Text.Json.Nodes;
+
+var builder = WebApplication.CreateBuilder(args);
+JsonNode? json;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
